6c7e61f0152d3cf2985be09e8439b9efcf1ce751
mibarbic <mibarbic@users.noreply.github.com>
2021-02-11 13:49:53 -0800

    WebGL troubleshooter implemented into index.html

--- scene.js
+++ scene.js
@@ -1,5 +1,6 @@
-import * as THREE from 'C:\Users\micanbar\OneDrive\Desktop\Three.js CORE\js\three.module.js';
-const scene = new THREE.Scene();
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
